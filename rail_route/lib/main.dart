950af85d24475af49f07874298d525c47031efc2
jess-icaa <jessicashalomin@gmail.com>
2026-01-07 14:32:35 +0530

    completed statelesswidget

--- rail_route/lib/main.dart
+++ rail_route/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/stateless_stateful_demo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,62 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Debug Demo',
+      title: 'Flutter Sprint 2 Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int count = 0;
-
-  void increment() {
-    setState(() {
-      count++;
-      debugPrint('Count updated to $count');
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hot Reload Demo'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'You have pressed the button this many times:',
-              style: TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-
-            /// 🔥 CHANGE THIS TEXT OR COLOR TO DEMONSTRATE HOT RELOAD
-            Text(
-              '$count',
-              style: const TextStyle(
-                fontSize: 48,
-                color: Colors.blue,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: increment,
-        child: const Icon(Icons.add),
-      ),
+      home: const StatelessStatefulDemo(),
     );
   }
 }
